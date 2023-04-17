cda35b56eccc562f492f458b4f0bb5e3d78e46e1
Willian Barbosa <willian@rxredefined.com>
2023-04-17 15:42:44 -0300

    Implemented type and mock data for users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+};
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      name: 'Willian',
+      username: 'willianrbarbosa',
+      password: '123456',
+    },
+    {
+      id: 2,
+      name: 'Pamela',
+      username: 'pamelaeduarda',
+      password: '654321',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
