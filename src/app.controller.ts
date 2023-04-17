d0f84afa877f96e07cd4a388bbe75bc3ea39e4ce
Willian Barbosa <willian@rxredefined.com>
2023-04-17 16:28:21 -0300

    Added Authenticated Guard to protected routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,21 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthenticatedGuard } from './auth/authenticated.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  //POST Login
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Request() req): any {
     return req.user;
   }
 
+  @UseGuards(AuthenticatedGuard)
   @Get('protected')
-  getHello(): string {
+  getHello(@Request() req): string {
     return this.appService.getHello();
   }
 }
