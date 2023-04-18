a2f1a9bc8bd6e4db007da4e9fbe0568f98ae7398
Willian Barbosa <willian@rxredefined.com>
2023-04-18 11:42:47 -0300

    Session login

--- src/auth/session.serializer.ts
+++ src/auth/session.serializer.ts
@@ -1,17 +1,23 @@
 import { PassportSerializer } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { User, UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
-  serializeUser(user: any, done: (err: Error, user: any) => void): any {
-    done(null, { id: user.id });
+  constructor(private readonly usersService: UsersService) {
+    super();
   }
-  deserializeUser(
+
+  serializeUser(user: any, done: (err: Error, user: any) => void): any {
+    done(null, { id: user.id, username: user.username });
+  }
+
+  async deserializeUser(
     payload: any,
-    done: (err: Error, paylod: string) => void,
-  ): any {
-    // const user = this.userService.findById(payload.id);
-    // done(null, user);
-    done(null, payload);
+    done: (err: Error, paylod: any) => void,
+  ): Promise<any> {
+    const user = await this.usersService.findById(payload.id);
+    const { password, username, ...rest } = user;
+    done(null, rest);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -27,4 +27,8 @@ export class UsersService {
   async findOne(username: string): Promise<User | undefined> {
     return this.users.find((user) => user.username === username);
   }
+
+  async findById(id: number): Promise<User | undefined> {
+    return this.users.find((user) => user.id === id);
+  }
 }
